cfc794a1560ca54995cd2fd8e4e5154804a66fcd
Daniel Kopaczel <dac@spyro-soft.com>
2025-07-07 21:32:28 +0200

    feat: add outputs to modules

--- terraform/modules/sql-server-and-database/outputs.tf
+++ terraform/modules/sql-server-and-database/outputs.tf
@@ -2,6 +2,10 @@ output "sql_server_id" {
   description = "The ID of the created Sql Server"
   value       = azurerm_mssql_server.mssql_server.id
 }
+output "sql_server_name" {
+  description = "The ID of the created Sql Server"
+  value       = azurerm_mssql_server.mssql_server.name
+}
 output "sql_server_fqdn" {
   description = "The Fully Qualified Domain Name of the created Sql Server"
   value       = azurerm_mssql_server.mssql_server.fully_qualified_domain_name
