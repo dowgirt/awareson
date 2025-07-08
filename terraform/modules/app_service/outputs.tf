5adfbf36dfffda9d57137240f507bd656f37c5f5
Daniel Kopaczel <dac@spyro-soft.com>
2025-07-08 11:43:15 +0200

    fix: Change user-assigned to system-assigned

--- terraform/modules/app_service/outputs.tf
+++ terraform/modules/app_service/outputs.tf
@@ -1,4 +1,4 @@
-output "app_service_principal_id" {
+output "identity_principal_id" {
   description = "Principal ID of the user-assigned identity used by the App Service"
   value       = azurerm_linux_web_app.web_app.identity[0].principal_id
 }
