890f4aa022e69b46a7e3cc334dd8f4b0d5e29eca
Daniel Kopaczel <dac@spyro-soft.com>
2025-07-08 15:15:47 +0200

    refactor: changes in deploy-app.yml

--- scripts/test-db-connection.js
+++ scripts/test-db-connection.js
@@ -3,8 +3,8 @@ const sql = require('mssql');
 (async () => {
 try {
     const config = {
-    user: process.env.sql-server-admin-username,
-    password: process.env.sql-server-admin-password,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
     server: process.env.DB_SERVER,
     database: process.env.DB_NAME,
     options: { encrypt: true }
